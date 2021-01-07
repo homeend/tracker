7ab513b5e29f82bb85a0ea9d760ca69290cf12fa
Michal R <homeend@wp.pl>
2021-01-08 00:17:11 +0100

    added basic index and state page structure

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,6 +1,5 @@
 <script context="module">
     export async function preload(page){
-        console.log(page);
         return {
             state: page.params['state']
         }
@@ -8,8 +7,13 @@
 </script>
 
 <script>
+    import CovidStat from '../components/CovidStat.svelte';
+    import CovidChart from '../components/CovidChart.svelte';
+    import TableContainer from '../components/TableContainer.svelte';
     export let state;
-
 </script>
 
 <h1>{state}</h1>
+
+<CovidStat />
+<CovidChart />
