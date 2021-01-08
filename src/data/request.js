14a15fe0a8ea98c2ad0b11759df1f62a41b18674
Michal R <homeend@wp.pl>
2021-01-08 02:01:04 +0100

    added error reponse on data load error

--- src/data/request.js
+++ src/data/request.js
@@ -5,6 +5,22 @@ import parser from './parser';
 const DATA_ENDPOINT = 'https://api.covidtracking.com/v1/us/current.json';
 
 export default async function usStats(){
-    const resp = await axios.get(DATA_ENDPOINT);
-    return parser.parseUsStats(resp.data);
+    try{
+        const resp = await axios.get(DATA_ENDPOINT);
+        return parser.parseUsStats(resp.data);
+    }
+    catch(e){
+        console.log(e);
+        const err_txt = 'temporarily unavailable';
+        return {
+            cases: err_txt,
+            death: err_txt,
+            recovered: err_txt,
+            ventilator: err_txt,
+            hospitalized: err_txt,
+            icu: err_txt,
+            tested: err_txt,
+            updated: moment().format('LLLL'),
+        };    
+    }
 }   
