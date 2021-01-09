601b816848bb519367c7d955ae92f8a15a6547dd
Michal R <homeend@wp.pl>
2021-01-09 19:13:19 +0100

    filter component created

--- src/components/TableContainer.svelte
+++ src/components/TableContainer.svelte
@@ -1,9 +1,11 @@
 <script>
     import TableFilter from './TableFilter.svelte';
     import Table from './Table.svelte';
+import stateNames from '../data/stateNames';
     export let states;
+    let sortBy = 'name';
+    let stateName = '';
     $: statesData = states;
 </script>
-
-<TableFilter />
+<TableFilter bind:stateName bind:sortBy/>
 <Table states={statesData} />
